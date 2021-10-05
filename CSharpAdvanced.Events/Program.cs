99614422df2760695aa84a367da13e8aaea63202
ahmed break <ahmed_break8@yahoo.com>
2021-10-05 14:39:09 +0200

    Events

---
+++ CSharpAdvanced.Events/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace CSharpAdvanced.Events
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var stock = new Stock("Amazon");
+            stock.Price = 100;
+
+            // Subscribe in Event
+            stock.OnPriceChanged += Stock_OnPriceChanged;
+
+            Console.WriteLine($"stock befor change price $ : { stock.Price}");
+
+            stock.ChangePriceBy(0.0);
+            stock.ChangePriceBy(0.4);
+            stock.ChangePriceBy(-0.2);
+
+            Console.WriteLine($"stock after change price $ : { stock.Price}");
+
+            // UnSubscribe in Event
+            stock.OnPriceChanged -= Stock_OnPriceChanged;
+
+
+            // another write Subscribe Method
+            stock.OnPriceChanged += (Stock stock, double oldPrice) =>
+            {
+                if (stock.Price > oldPrice)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{stock.Name} : {stock.Price}");
+                }
+                else if (stock.Price < oldPrice)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{stock.Name} : {stock.Price}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{stock.Name} : {stock.Price}");
+
+                }
+            };
+            // ---------------------------------------------------------
+            stock.ChangePriceBy(0.4);
+            Console.ReadKey();
+        }
+
+        private static void Stock_OnPriceChanged(Stock stock, double oldPrice)
+        {
+            if (stock.Price > oldPrice)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{stock.Name} : {stock.Price}");
+            }
+            else if (stock.Price < oldPrice)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{stock.Name} : {stock.Price}");
+
+            }
+            else
+            {
+                Console.WriteLine($"{stock.Name} : {stock.Price}");
+
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced.Events/Stock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.Events
+{
+    public class Stock
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Stock(string name)
+        {
+            this.Name = name;
+        }
+
+        // step 1
+        public delegate void StockPriceChangeHandler(Stock stock, double oldPrice);
+
+        // step 2
+        public event StockPriceChangeHandler OnPriceChanged;
+
+        public void ChangePriceBy(double prcent)
+        {
+            var oldPrice = this.Price;
+            this.Price += Math.Round(Price * prcent, 2);
+
+            if (OnPriceChanged !=null) // make sure there Subscriber
+            {
+                // step 3
+                OnPriceChanged(this, oldPrice);
+            }
+        }
+    }
+}
