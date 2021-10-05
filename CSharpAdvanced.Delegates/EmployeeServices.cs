f9e6d6bc7fd3caa05205f26678ae6eb00e1fe1e3
ahmed break <ahmed_break8@yahoo.com>
2021-10-05 12:04:26 +0200

    Add project files.

---
+++ CSharpAdvanced.Delegates/EmployeeServices.cs
@@ -0,0 +1,28 @@
+﻿using CSharpAdvanced.Delegates.Models;
+using System.Collections.Generic;
+
+namespace CSharpAdvanced.Delegates
+{
+    public class EmployeeServices
+    {
+        public static List<Employee> Employees = new List<Employee>();
+
+        public EmployeeServices()
+        {
+            for (int i = 0; i < 51; i++)
+            {
+                Employees.Add(new Employee
+                {
+                    Id = i,
+                    Name = $"Employee_NO_ {i}",
+                    Salary = 1000 * (i + i)
+                });
+            }
+        }
+
+        public void PrintEmployeeWithCondition()
+        {
+
+        }
+    }
+}
---
+++ CSharpAdvanced.Delegates/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpAdvanced.Delegates
+{
+    using System;
+
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
