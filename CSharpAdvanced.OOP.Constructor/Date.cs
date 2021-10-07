5c06e61de413e30c1400b06021db4f263a2f1015
ahmed break <ahmed_break8@yahoo.com>
2021-10-07 23:14:38 +0200

    OOP.Constructor

---
+++ CSharpAdvanced.OOP.Constructor/Date.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.OOP.Constructor
+{
+    public class Date
+    {
+        private static readonly int[] dayesToMoth365 = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        private static readonly int[] dayesToMoth366 = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        private readonly int day = 1;
+        private readonly int moth = 1;
+        private readonly int year = 1;
+
+
+        public Date(int day, int moth , int year)
+        {
+            var isleap = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+            if (year >= 1 && year <= 9999 && moth >= 1 && moth <= 12)
+            {
+                int[] dayes = isleap ? dayesToMoth366 : dayesToMoth365;
+                if (day >= 1 && day <= dayes[moth])
+                {
+                    this.day = day;
+                    this.moth = moth;
+                    this.year = year;
+                }
+
+            }
+        }
+        public Date(int year) : this (1,1,year)
+        {
+
+        }
+        public string GetDate()
+        {
+            return $"{this.day.ToString().PadLeft(2, '0')}/{this.moth.ToString().PadLeft(2, '0')}/{this.year.ToString().PadLeft(4, '0')}";
+        }
+
+    }
+}
---
+++ CSharpAdvanced.OOP.Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharpAdvanced.OOP.Constructor
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var d1 = new Date(29,2,1999);
+            var d2 = new Date( 1999);
+
+            Console.WriteLine(d2.GetDate());
+
+            var emp1 = new Employee();
+            emp1.Id = 100;
+            emp1.Name = "ahmed";
+            Console.WriteLine($"{emp1.Id} - {emp1.Name}" );
+            var emp2 = new Employee { Id = 105, Name = "Break" };
+            Console.WriteLine($"{emp2.Id} - {emp2.Name}");
+
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
