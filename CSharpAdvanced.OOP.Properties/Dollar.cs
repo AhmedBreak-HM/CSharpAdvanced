2820c347046f5e53588c59e4d9990dc79c14f529
ahmed break <ahmed_break8@yahoo.com>
2021-10-08 00:53:34 +0200

    OOP.Properties

---
+++ CSharpAdvanced.OOP.Properties/Dollar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.OOP.Properties
+{
+    public class Dollar
+    {
+        private double amount;
+
+        public double Price { get; set; } = 95;
+
+        public double Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                // Validation
+                if (value < 0) amount = 0;
+                else amount = Math.Round(value, 2);
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced.OOP.Properties/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpAdvanced.OOP.Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d1 = new Dollar { Amount = -55 };
+            Console.WriteLine(d1.Amount);
+            Console.WriteLine(d1.Price);
+
+
+        }
+    }
+}
