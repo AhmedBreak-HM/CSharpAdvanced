d82ae78f8498826673a704725e10a4fbd20f51e6
ahmed break <ahmed_break8@yahoo.com>
2021-10-08 18:59:29 +0200

    OOP.Indexers

---
+++ CSharpAdvanced.OOP.Indexers/IPAdress.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.OOP.Indexers
+{
+    public class IPAdress
+    {
+
+        public int[] Segments { get; set; } = new int[4];
+
+        public int this [int index]
+        {
+            get => Segments[index];
+            set { Segments[index] = value; }
+        }
+
+        public IPAdress(string ipAdress)
+        {
+            var segm = ipAdress.Split('.');
+
+            for (int i = 0; i <= segm.Length; i++)
+            {
+                Segments[i] = int.Parse(segm[i]) ;
+            }
+
+        }
+        public IPAdress(int segment1,int segment2, int segment3, int segment4)
+        {
+            Segments[0] = segment1;
+            Segments[1] = segment2;
+            Segments[2] = segment3;
+            Segments[3] = segment4;
+
+        }
+
+        public string GetIPAdress() => string.Join('.', Segments);
+
+    }
+}
---
+++ CSharpAdvanced.OOP.Indexers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpAdvanced.OOP.Indexers
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip1 = new IPAdress(112,114,55,32);
+            var ip2 = new IPAdress("112.112.112.112");
+            var fristSegment = ip1[1];
+            Console.WriteLine(ip1.GetIPAdress());
+            Console.WriteLine(fristSegment);
+            Console.WriteLine(ip2.GetIPAdress());
+
+
+
+
+        }
+    }
+}
